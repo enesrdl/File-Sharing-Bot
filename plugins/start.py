ce86bb5a630e9b01101c2544c71a0f1d4a270079
Enes Erdil <86965500+enesrdl@users.noreply.github.com>
2023-07-06 16:10:41 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -119,6 +119,13 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
     
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
+    buttons = [	
+        [	
+            InlineKeyboardButton(		
+                "Join Channel",		
+                url = client.invitelink)	
+        ]	
+    ]
     try:
         buttons.append(
             [
