d04caaee0e37e158caa2e89b502d781705709130
Enes Erdil <86965500+enesrdl@users.noreply.github.com>
2023-07-06 20:22:18 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -146,7 +146,6 @@ async def not_joined(client: Client, message: Message):
                 mention = message.from_user.mention,
                 id = message.from_user.id
             ),
-        reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
         disable_web_page_preview = True
     )
